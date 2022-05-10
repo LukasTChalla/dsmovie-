1ee181a35e063508854043aa71f4e9a2cddbefcc
T'Challa <tchalladevs@gmail.com>
2022-05-10 09:40:31 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/lukastchalla/dsmovie/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukastchalla.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
